514ad7e19e6e7ce5720a69789d958e0183ff0db0
kuckjwi0928 <skyblue0928@naver.com>
2021-06-19 22:35:21 +0900

    first commit

---
+++ src/main/kotlin/loop.kt
@@ -0,0 +1,45 @@
+import java.util.*
+
+fun main() {
+    for (i in 1..100) {
+        print(fizzBuzz(i))
+    }
+    println()
+    for (i in 100 downTo 1 step 2) {
+        print(fizzBuzz(i))
+    }
+    println()
+    mapIteration()
+    listIteration()
+    println(isLetter('a'))
+    println(isNotDigit('a'))
+}
+
+fun fizzBuzz(i: Int) = when {
+    i % 15 == 0 -> "FizzBuzz "
+    i % 3 == 0 -> "Fizz "
+    i % 5 == 0 -> "Buzz "
+    else -> "$i "
+}
+
+fun mapIteration() {
+    val binaryReps = TreeMap<Char, String>()
+    for (c in 'A'..'F') {
+        val binary = Integer.toBinaryString(c.toInt());
+        binaryReps[c] = binary;
+    }
+    for ((letter, binary) in binaryReps) {
+        println("$letter = $binary")
+    }
+}
+
+fun listIteration() {
+    val list = listOf("10", "11", "1001")
+    for ((index, element) in list.withIndex()) {
+        println("$index: $element")
+    }
+}
+
+fun isLetter(c: Char) = c in 'a'..'z' || c in 'A'..'Z'
+
+fun isNotDigit(c: Char) = c !in '0'..'9'
